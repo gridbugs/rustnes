b91089130eb0e94baefcef4962b5dd1162fe880f
Stephen Sherratt <stephen@sherra.tt>
2016-09-27 10:45:40 +1000

    Added read16_le and read32_le helper methods

--- src/addressable.rs
+++ src/addressable.rs
@@ -16,6 +16,22 @@ pub enum Error {
 pub trait CpuAddressable {
     fn read8(&mut self, address: Address) -> Result<u8>;
     fn write8(&mut self, address: Address, data: u8) -> Result<()>;
+
+    fn read16_le(&mut self, address: Address) -> Result<u16> {
+        let lo = try!(self.read8(address)) as u16;
+        let hi = try!(self.read8(address.wrapping_add(1))) as u16;
+
+        Ok((hi << 8) | lo)
+    }
+
+    fn read32_le(&mut self, address: Address) -> Result<u32> {
+        let a = try!(self.read8(address)) as u32;
+        let b = try!(self.read8(address.wrapping_add(1))) as u32;
+        let c = try!(self.read8(address.wrapping_add(2))) as u32;
+        let d = try!(self.read8(address.wrapping_add(3))) as u32;
+
+        Ok((d << 24) | (c << 16) | (b << 8) | a)
+    }
 }
 
 pub trait PpuAddressable {
